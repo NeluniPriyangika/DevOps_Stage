d944419f5df094253f1b4e94036c2ada6fb1d7e0
NeluniPriyangika <96222@fhss.sjp.ac.lk>
2023-04-19 14:45:45 +0530

    adding the roadmap and edited the learning page

--- src/pages/learning/Learning.jsx
+++ src/pages/learning/Learning.jsx
@@ -4,7 +4,108 @@ import './learning.css';
 function Learning() {
   return (
     <div>
-      roadmap
+       <div class="main">
+        <h3 class="heading">DevOps Roadmap</h3>
+
+        <div class="container">
+            <ul>
+                <li>
+                    <h3 class="title">1). Learn programming languages</h3>
+                    <p>In here you have to learn some recomended languages which are realated to the DevOps</p>
+                    <div className='buttonDiv'>
+                      <button class="button-82-pushable" >
+                        <span class="button-82-shadow"></span>
+                        <span class="button-82-edge"></span>
+                        <span class="button-82-front text">Python</span>
+                      </button>
+                      <button class="button-82-pushable">
+                        <span class="button-82-shadow"></span>
+                        <span class="button-82-edge"></span>
+                        <span class="button-82-front text">Golang</span>
+                      </button>
+                      <button class="button-82-pushable">
+                        <span class="button-82-shadow"></span>
+                        <span class="button-82-edge"></span>
+                        <span class="button-82-front text">Javascript</span>
+                      </button>
+                      <button class="button-82-pushable">
+                        <span class="button-82-shadow"></span>
+                        <span class="button-82-edge"></span>
+                        <span class="button-82-front text">Ruby</span>
+                      </button>
+                    </div>
+                    
+                    <span class="circle"></span>
+                    <span class="date">Get Start..!</span>
+                </li>
+                <li>
+                    <h3 class="title">2). Server Administration</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eaque quidem esse? Incidunt, odit beatae?</p>
+                    <div className='buttonDiv'>
+                      <button class="button-82-pushable" >
+                        <span class="button-82-shadow"></span>
+                        <span class="button-82-edge"></span>
+                        <span class="button-82-front text">Linux</span>
+                      </button>
+                      <button class="button-82-pushable">
+                        <span class="button-82-shadow"></span>
+                        <span class="button-82-edge"></span>
+                        <span class="button-82-front text">Unix</span>
+                      </button>
+                      <button class="button-82-pushable">
+                        <span class="button-82-shadow"></span>
+                        <span class="button-82-edge"></span>
+                        <span class="button-82-front text">Windows</span>
+                      </button>
+                    </div>
+                    <span class="circle"></span>
+                    <span class="date">Second Step</span>
+                </li>
+                <li>
+                    <h3 class="title">3). Network and Security</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eaque quidem esse? Incidunt, odit beatae?</p>
+                    <div className='buttonDiv'>
+                      <button class="button-82-pushable" >
+                        <span class="button-82-shadow"></span>
+                        <span class="button-82-edge"></span>
+                        <span class="button-82-front text">TCP/IP Fundermental</span>
+                      </button>
+                      <button class="button-82-pushable">
+                        <span class="button-82-shadow"></span>
+                        <span class="button-82-edge"></span>
+                        <span class="button-82-front text">Protocols: DNS, HTTP/s,FTP,SSL...</span>
+                      </button>
+                    </div>
+                    <span class="circle"></span>
+                    <span class="date">Third Step</span>
+                </li>
+                <li>
+                    <h3 class="title">4). Servers</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem eaque quidem esse? Incidunt, odit beatae?</p>
+                    <div className='buttonDiv'>
+                      <button class="button-82-pushable" >
+                        <span class="button-82-shadow"></span>
+                        <span class="button-82-edge"></span>
+                        <span class="button-82-front text">Web server</span>
+                      </button>
+                      <button class="button-82-pushable">
+                        <span class="button-82-shadow"></span>
+                        <span class="button-82-edge"></span>
+                        <span class="button-82-front text">Caching</span>
+                      </button>
+                      <button class="button-82-pushable">
+                        <span class="button-82-shadow"></span>
+                        <span class="button-82-edge"></span>
+                        <span class="button-82-front text">Database</span>
+                      </button>
+                    </div>
+                    <span class="circle"></span>
+                    <span class="date">Fourth Step</span>
+                </li>
+            </ul>
+        </div>
+    </div>
+
     </div>
   )
 }
